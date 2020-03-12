593d6b31b981df6175fbc96751e881f449968a27
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-12 16:51:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,16 @@
 # Make your shoe class here!
 class Shoe
  
+ attr_accessor :color, :size, :material, :condition
+ 
  attr_reader :brand
  
   def initialize(brand = "Adidas")
     @brand = brand
   end
  
+ def cobble
+   puts ""
 
  
   
